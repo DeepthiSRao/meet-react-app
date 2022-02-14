09d54cadf28f4ba41beacc2004578e095e986c58
Deepthi Srinidhi Rao <deepthi2609@gmail.com>
2022-02-14 13:21:53 -0800

    app perform monitoring tool added

--- src/_test_/EndToEnd.test.js
+++ src/_test_/EndToEnd.test.js
@@ -36,3 +36,38 @@ describe('show/hide an event details', () => {
         expect(eventDetails).toBeNull();
     });
 });
+
+/* describe('Filter event by city', () => {
+    let browser, page;
+
+    beforeAll(async () => {
+        jest.setTimeout(50000);
+        browser = await puppeteer.launch({
+            headless: true,
+            slowMo: 250, // slow down by 250ms
+            ignoreDefaultArgs: ['--disable-extensions'] // ignores default setting that causes timeout errors
+        });
+        page = await browser.newPage();
+        await page.goto('http://localhost:3000/'); 
+        await page.waitForSelector('.event'
+        
+        );
+        await page.waitForSelector('.city');
+    });
+
+    test('When user hasn’t searched for a city, show upcoming events from all cities.', async() => {    
+        const event = await page.$('.event');
+        expect(event).toBeDefined();
+    }); 
+
+    test('User should see a list of suggestions when they search for a city', async () => {
+        await page.focus('.city');
+        await page.keyboard.type('.city', 'Berlin, Germany');
+        const suggestionsList = await page.$('.suggestions li');
+        expect(suggestionsList).toHaveLength(2);
+    });
+
+    afterAll(() => {
+        browser.close();
+    });
+});*/
--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+
+// for monitoring application with atatus 
+atatus.config('2ae699c967d647f99df4fa77b894ad33').install();
 
 ReactDOM.render(
   <React.StrictMode>

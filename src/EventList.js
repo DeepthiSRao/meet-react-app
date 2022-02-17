4982168ccd9f8dda5a57464976b8636367c2bb37
Deepthi Srinidhi Rao <deepthi2609@gmail.com>
2022-02-17 13:29:57 -0800

    offline alert  message feature implemented

--- src/EventList.js
+++ src/EventList.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import Event from './Event';
+import { WarningAlert } from './Alert';
 
 const EventList = ({ events }) => {
     return ( 
-        <ul className="event-list">
-            {
-                events.map(event => 
-                    <li key={event.id} >
-                        <Event event={event} />
-                    </li>
-            )}
-        </ul>
+        <div>
+            { !navigator.onLine ?
+                <WarningAlert text="Your offline. Events data are loaded from cache!" />
+                : ''
+            }
+            <ul className="event-list">
+                {
+                    events.map(event => 
+                        <li key={event.id} >
+                            <Event event={event} />
+                        </li>
+                )}
+            </ul>
+        </div>
     );
 }
 
